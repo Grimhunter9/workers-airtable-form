8b0be2030dfb2e95d0a60cf6823647869a4fb27b
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2023-01-20 18:17:34 +0100

    me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { PencilAltIcon } from '@heroicons/react/outline'
+
 
 import Form from './Form'
 
@@ -14,150 +14,18 @@ const ExternalLink = ({ children, hoverColor = "text-teal-800", href }) =>
 
 export default function FormPage() {
   return (
-    <div className="min-h-screen bg-white">
-      <main className="overflow-hidden">
+    <div className=" bg-white">
+      
         {/* Contact section */}
         <section className="relative bg-white pb-24" aria-labelledby="contactHeading">
-          <div className="absolute w-full h-1/2 bg-warm-gray-50" aria-hidden="true" />
-          {/* Decorative dot pattern */}
-          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <svg
-              className="absolute z-0 top-0 right-0 transform -translate-y-16 translate-x-1/2 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-              aria-hidden="true"
-            >
-              <defs>
-                <pattern
-                  id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect x={0} y={0} width={4} height={4} className="text-warm-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-            </svg>
-          </div>
+          
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="relative bg-white shadow-xl">
               <h2 id="contactHeading" className="sr-only">
                 Contact us
               </h2>
-
               <div className="grid grid-cols-1 lg:grid-cols-3">
                 {/* Contact information */}
-                <div className="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:p-12">
-                  {/* Decorative angle backgrounds */}
-                  <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
-                    <svg
-                      className="absolute inset-0 w-full h-full"
-                      width={343}
-                      height={388}
-                      viewBox="0 0 343 388"
-                      fill="none"
-                      preserveAspectRatio="xMidYMid slice"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
-                        fill="url(#linear1)"
-                        fillOpacity=".1"
-                      />
-                      <defs>
-                        <linearGradient
-                          id="linear1"
-                          x1="254.553"
-                          y1="107.554"
-                          x2="961.66"
-                          y2="814.66"
-                          gradientUnits="userSpaceOnUse"
-                        >
-                          <stop stopColor="#fff" />
-                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                        </linearGradient>
-                      </defs>
-                    </svg>
-                  </div>
-                  <div
-                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
-                    aria-hidden="true"
-                  >
-                    <svg
-                      className="absolute inset-0 w-full h-full"
-                      width={359}
-                      height={339}
-                      viewBox="0 0 359 339"
-                      fill="none"
-                      preserveAspectRatio="xMidYMid slice"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
-                        fill="url(#linear2)"
-                        fillOpacity=".1"
-                      />
-                      <defs>
-                        <linearGradient
-                          id="linear2"
-                          x1="192.553"
-                          y1="28.553"
-                          x2="899.66"
-                          y2="735.66"
-                          gradientUnits="userSpaceOnUse"
-                        >
-                          <stop stopColor="#fff" />
-                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                        </linearGradient>
-                      </defs>
-                    </svg>
-                  </div>
-                  <div
-                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
-                    aria-hidden="true"
-                  >
-                    <svg
-                      className="absolute inset-0 w-full h-full"
-                      width={160}
-                      height={678}
-                      viewBox="0 0 160 678"
-                      fill="none"
-                      preserveAspectRatio="xMidYMid slice"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
-                        fill="url(#linear3)"
-                        fillOpacity=".1"
-                      />
-                      <defs>
-                        <linearGradient
-                          id="linear3"
-                          x1="192.553"
-                          y1="325.553"
-                          x2="899.66"
-                          y2="1032.66"
-                          gradientUnits="userSpaceOnUse"
-                        >
-                          <stop stopColor="#fff" />
-                          <stop offset={1} stopColor="#fff" stopOpacity={0} />
-                        </linearGradient>
-                      </defs>
-                    </svg>
-                  </div>
-                  <h3 className="text-lg font-medium text-white">Example form</h3>
-                  <p className="mt-6 text-base text-teal-50 max-w-3xl">
-                    A fully-featured HTML5 <code>form</code> element that submits data to a serverless function.
-                    This data is added to an <ExternalLink hoverColor="text-teal-300" href="https://airtable.com">Airtable</ExternalLink> table, which can be connected with various integrations and extensions to notify
-                    you on new submissions and changes.
-                  </p>
-                </div>
-
                 {/* Contact form */}
                 <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
                   <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
@@ -167,7 +35,7 @@ export default function FormPage() {
             </div>
           </div>
         </section>
-      </main>
+    
     </div>
   )
 }
--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -3,38 +3,6 @@ const SERVERLESS_FN_URL = "https://workers-airtable-form.workerservicehandler.wo
 export default () => (
   <form action={SERVERLESS_FN_URL} method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
     <div>
-      <label htmlFor="first_name" className="block text-sm font-medium text-warm-gray-900">
-        First name
-      </label>
-      <div className="mt-1">
-        <input
-          type="text"
-          name="first_name"
-          id="first_name"
-          autoComplete="given-name"
-          placeholder="Ellen"
-          required
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-        />
-      </div>
-    </div>
-    <div>
-      <label htmlFor="last_name" className="block text-sm font-medium text-warm-gray-900">
-        Last name
-      </label>
-      <div className="mt-1">
-        <input
-          type="text"
-          name="last_name"
-          id="last_name"
-          autoComplete="family-name"
-          placeholder="Ripley"
-          required
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-        />
-      </div>
-    </div>
-    <div>
       <label htmlFor="email" className="block text-sm font-medium text-warm-gray-900">
         Email
       </label>
@@ -51,50 +19,8 @@ export default () => (
       </div>
     </div>
     <div>
-      <div className="flex justify-between">
-        <label htmlFor="phone" className="block text-sm font-medium text-warm-gray-900">
-          Phone
-        </label>
-        <span id="phone-optional" className="text-sm text-warm-gray-500">
-          Optional
-        </span>
-      </div>
-      <div className="mt-1">
-        <input
-          type="text"
-          name="phone"
-          id="phone"
-          autoComplete="tel"
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-          placeholder="+1 (123) 456-7890"
-          aria-describedby="phone-optional"
-        />
-      </div>
     </div>
     <div className="sm:col-span-2">
-      <label htmlFor="subject" className="block text-sm font-medium text-warm-gray-900">
-        Subject
-      </label>
-      <div className="mt-1">
-        <input
-          type="text"
-          name="subject"
-          id="subject"
-          placeholder="Your example subject"
-          required
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-        />
-      </div>
-    </div>
-    <div className="sm:col-span-2">
-      <div className="flex justify-between">
-        <label htmlFor="message" className="block text-sm font-medium text-warm-gray-900">
-          Message
-        </label>
-        <span id="message-max" className="text-sm text-warm-gray-500">
-          Max. 500 characters
-        </span>
-      </div>
       <div className="mt-1">
         <textarea
           id="message"
